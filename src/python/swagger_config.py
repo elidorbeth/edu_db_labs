ea039712612f7c2f839544ba28dea5193bac06b0
Andriy <sukhov.andriy@lll.kpi.ua>
2025-05-26 21:16:38 +0300

    src

---
+++ src/python/swagger_config.py
@@ -0,0 +1,11 @@
+from flask_restx import Api
+
+def init_api(app):
+    api = Api(
+        app,
+        version='1.0',
+        title='FORMSYS API',
+        description='Документація REST API для системи опитувань експертів',
+        doc='/docs'
+    )
+    return api
